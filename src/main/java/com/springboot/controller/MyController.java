7c2ceb0b42f98218fa3d0e3e7e3e2cdfc62ef268
Awadhesh-Gupta <22054295@kiit.ac.in>
2025-06-07 01:50:22 +0530

    Fixing bugs and starting on port 9090 instead of 8080

--- src/main/java/com/springboot/controller/MyController.java
+++ src/main/java/com/springboot/controller/MyController.java
@@ -1,4 +1,4 @@
-package SpringBoot.Controller;
+package com.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
