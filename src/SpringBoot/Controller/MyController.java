c461ee5fbc91260cae573b41591a0821f7a7fed8
Awadhesh-Gupta <22054295@kiit.ac.in>
2025-06-06 23:57:59 +0530

    Initiating SpringBoot

---
+++ src/SpringBoot/Controller/MyController.java
@@ -0,0 +1,15 @@
+package SpringBoot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myapi")
+public class MyController {
+
+    @GetMapping("/firstapi")
+    public String getData() {
+        return "Hello Spring Boot!";
+    }
+}
