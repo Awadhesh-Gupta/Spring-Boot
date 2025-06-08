57ec11bb89049052f3f08ef150e5b4b7a085710f
Awadhesh-Gupta <22054295@kiit.ac.in>
2025-06-09 01:19:14 +0530

    Implementions of Getter and Setter i.e Getting and Setting the parameters value for PropertyEditor

---
+++ src/main/java/com/springboot/model/GetterSetterForController.java
@@ -0,0 +1,32 @@
+package com.springboot.model;
+
+public class GetterSetterForController {
+    private String firstname;
+    private String lastname;
+    private int age;
+
+    // Getters and Setters
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
